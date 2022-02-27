6ac4c38b8bde50445afcc0bf52f6bcee7204db59
Andrew <aschoch10@gmail.com>
2022-02-27 08:42:46 -0800

    habit table in sql, habit model in flask app

--- flask_app/models/habit.py
+++ flask_app/models/habit.py
@@ -1,1 +1,27 @@
-# to do code some more stuff I would imagine 
+# to do code some more stuff I would imagine import re
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_bcrypt import Bcrypt
+from flask_app import app
+
+bcrypt = Bcrypt(app)
+
+class Habit:
+    schema = ("habittracker_schema")
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['first_name']
+        self.description = data['description']
+        self.streak_count = data['streak_count']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.recipes = []
+
+# to do create form and controller to route to this
+    @classmethod
+    def getById(cls, data):
+        query = "SELECT * FROM habits WHERE email = %(id)s;"
+        results = connectToMySQL(cls.schema).query_db(query, data)
+        if len(results) < 1:
+            return False
+        return Habit(results[0])
