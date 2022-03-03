3dade0365c8d7bfff6f55be3579f1051c54949f7
Andrew <aschoch10@gmail.com>
2022-03-03 11:27:46 -0800

    work around controller all in one file, trying to get db connected properly

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -50,14 +50,31 @@ def dashboard():
         return redirect("/")
     return render_template ("dashboard.html", 
     logged_in_user = User.getByID({"id": session["uuid"]}),
-    # all_habits = Habit.readAll()
+    all_habits = Habit.readAll()
     )
 
 # only can acces user controller from html buttons
-# to do connect habit controller 
+# to do >>fix
 @app.route("/test")
-def newHabit():
+def test():
     return render_template("test.html")
+
+@app.route("/habits/new")
+def newHabit():
+    return render_template("new_habit.html")
+
+
+@app.route("/habits/create", methods = ['POST'])
+def createHabit():
+    # validations for form input can be used after fixing validate method in habit.py
+    # if not Habit.validate(request.form):
+    #     return redirect("/habits/new")
+    data = {
+        **request.form,
+        "user_id": session['uuid']
+    }
+    Habit.create(data)
+    return redirect('/dashboard')
 
 
 
--- flask_app/models/habit.py
+++ flask_app/models/habit.py
@@ -1,4 +1,3 @@
-# to do code some more stuff I would imagine import re
 from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_bcrypt import Bcrypt
@@ -10,12 +9,11 @@ class Habit:
     schema = ("habittracker_schema")
     def __init__(self, data):
         self.id = data['id']
-        self.name = data['first_name']
+        self.name = data['name']
         self.description = data['description']
         self.streak_count = data['streak_count']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        self.recipes = []
 
 
     @classmethod
@@ -36,8 +34,25 @@ class Habit:
             habits.append(Habit(row))
         return habits
 
-
+# todo fix schema in workbench
     @classmethod
     def create(cls, data):
-        query = "INSERT into habits (user_id, name, description, streak_count) VALUES (%(user_id)s, %(name)s, %(description)s, %(streak_count)s);"
+        query = "INSERT into habits (users_id, name, description, streak_count) VALUES (%(user_id)s, %(name)s, %(description)s, %(streak_count)s);"
         return connectToMySQL(cls.schema).query_db(query, data)
+
+# todo add validations for form input
+    # @staticmethod
+    # def validate(post_data):
+    #     is_valid = True
+
+    #     if len(post_data['title']) <3:
+    #         flash("Title must be longer than 3 characters")
+    #         is_valid = False
+    #     if len(post_data['network']) < 3:
+    #         flash("Description must be longer than 3 characters")
+    #         is_valid = False
+    #     if len(post_data['description']) <3:
+    #         flash("Instructions must be longer than 3 characters")
+    #         is_valid = False
+
+    #     return is_valid
