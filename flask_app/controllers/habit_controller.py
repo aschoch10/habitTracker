b4f3d2372e92a63fe68a9e4760e40008b72405b5
Andrew <aschoch10@gmail.com>
2022-06-29 15:18:19 -0700

    fixed 1/day counter still need to format html dates

--- flask_app/controllers/habit_controller.py
+++ flask_app/controllers/habit_controller.py
@@ -3,7 +3,7 @@ from flask import render_template, request, redirect, session, flash
 from flask_app import app
 from flask_app.models.user import User
 from flask_app.models.habit import Habit
-from datetime import datetime, date, timedelta
+from datetime import datetime, date, timedelta, time
 
 
 
@@ -34,13 +34,6 @@ def createHabit():
     return redirect('/dashboard')
 
 
-@app.route("/habits/<int:id>/edit")
-def editShow(id):
-    return render_template("test.html", 
-    habit = Habit.readOne({"id": id})
-    )
-
-
 @app.route('/habits/<int:id>/increase', methods = ['POST'])
 def increase(id):
     data = {
@@ -49,17 +42,15 @@ def increase(id):
         "id":id,
         "streak_count": int(request.form['streak_count']) +1,
     }
-    Habit.update(data)
-    print("updated at" + data['updated_at'])
-    print(timedelta(hours=12))
-    print(datetime.now())
-    if "time" not in session:
-        session["time"] = datetime.now()
-    # if  data['updated_at'] + timedelta(hours=12) > session['time']:
-        # print("it has been more than twelve hours")
-    else: print ("it hasn't been more than twelve hours")
+    # convert [updated_at] to datetime object
+    # print(datetime.strptime(data['updated_at'], "%Y-%m-%d %H:%M:%S"))
+    # print(type(timedelta(hours=12)) )
+    # print(type(datetime.now()))
+    if(datetime.strptime(data['updated_at'], "%Y-%m-%d %H:%M:%S")) < (datetime.now()) - (timedelta(hours=12)):
+        print("it HAS been more than tweleve hours your streak has been updated")
+        Habit.update(data)
+    else: print ("It hasn't been more than twelve hours")
         # data['streak_count']+1
-    Habit.update(data)
 
     return redirect ("/dashboard")
 
